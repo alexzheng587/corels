41374a6db7aad32d417622026ccb9938dddd833f
nlarusstone <nlarusstone@college.harvard.edu>
2017-02-27 01:25:19 -0500

    Changed permutation map to inheritance structure.

--- src/pmap.hh
+++ src/pmap.hh
@@ -9,7 +9,9 @@
  */
 struct prefix_key {
     unsigned short *key;
-
+    prefix_key(unsigned short* k) {
+        key = k;
+    }
     bool operator==(const prefix_key& other) const {
         if (key[0] != other.key[0])
             return false;
@@ -41,18 +43,16 @@ struct captured_key {
 #ifndef GMP
     short len;
 #endif
-
     bool operator==(const captured_key& other) const {
 #ifdef GMP
-		return !mpz_cmp(other.key, key);
+        return !mpz_cmp(other.key, key);
 #else
-		size_t nentries = (len + BITS_PER_ENTRY - 1)/BITS_PER_ENTRY;
-		for (size_t i = 0; i < nentries; i++)
-			if (other.key[i] != key[i])
-				return false;
-		return true;
+        size_t nentries = (len + BITS_PER_ENTRY - 1)/BITS_PER_ENTRY;
+        for (size_t i = 0; i < nentries; i++)
+            if (other.key[i] != key[i])
+                return false;
+        return true;
 #endif
-
     }
 };
 
@@ -75,31 +75,170 @@ struct captured_hash {
     }
 };
 
-typedef std::unordered_map<struct prefix_key, std::pair<double, unsigned char*>, prefix_hash> PrefixPermutationMap;
-typedef std::unordered_map<struct captured_key, std::pair<std::vector<unsigned short>, double>, captured_hash> CapturedPermutationMap;
-
-template<class P>
-using pmap_garbage_collect_signature = void (*)(P*, size_t);
-
-void bfs_prefix_map_garbage_collect(PrefixPermutationMap* p, size_t min_length);
-void prefix_map_garbage_collect(PrefixPermutationMap* p, size_t min_length);
-void captured_map_garbage_collect(CapturedPermutationMap* p, size_t min_length);
-
-template<class N, class P>
+/*template<class N>
 using permutation_insert_signature = N* (*)(construct_signature<N>, unsigned short, size_t, bool, bool, 
                                             double, double, N* parent, int, int, int, double, double, CacheTree<N>*, VECTOR,
-                                            std::vector<unsigned short>, P*);
+                                            std::vector<unsigned short>, PermutationMap*);
+                                            */
 
 template<class N>
-N* prefix_permutation_insert(construct_signature<N> construct_policy, unsigned short new_rule,
-                        size_t nrules, bool prediction, bool default_prediction, double lower_bound,
-                        double objective, N* parent, int num_not_captured, int nsamples, int len_prefix,
-                        double c, double minority, CacheTree<N>* tree, VECTOR not_captured,
-                        std::vector<unsigned short>, PrefixPermutationMap* p);
+class PermutationMap {
+    public:
+        virtual size_t size() {
+            return 0;
+        }
+        virtual N* insert (construct_signature<N> construct_policy, unsigned short new_rule,
+                             size_t nrules, bool prediction, bool default_prediction, double lower_bound,
+                             double objective, N* parent, int num_not_captured, int nsamples, int len_prefix,
+                             double c, double minority, CacheTree<N>* tree, VECTOR not_captured,
+                             std::vector<unsigned short> parent_prefix)
+        {
+            return construct_policy(new_rule, nrules, prediction,
+                                    default_prediction, lower_bound, objective,
+                                    parent, num_not_captured, nsamples,
+                                    len_prefix, c, minority);
+        }
+};
 
 template<class N>
-N* captured_permutation_insert(construct_signature<N> construct_policy, unsigned short new_rule,
-                        size_t nrules, bool prediction, bool default_prediction, double lower_bound,
-                        double objective, N* parent, int num_not_captured, int nsamples, int len_prefix,
-                        double c, double minority, CacheTree<N>* tree, VECTOR not_captured,
-                        std::vector<unsigned short>, CapturedPermutationMap* p);
+class PrefixPermutationMap : public PermutationMap<N> {
+	public:
+        PrefixPermutationMap ();
+        size_t size() {
+            return pmap->size();
+        }
+        N* insert (construct_signature<N> construct_policy, unsigned short new_rule,
+					 size_t nrules, bool prediction, bool default_prediction, double lower_bound,
+					 double objective, N* parent, int num_not_captured, int nsamples, int len_prefix,
+					 double c, double minority, CacheTree<N>* tree, VECTOR not_captured,
+					 std::vector<unsigned short> parent_prefix) override
+		{
+            parent_prefix.push_back(new_rule);
+
+            unsigned char* ordered = (unsigned char*) malloc(sizeof(unsigned char) * (len_prefix + 1));
+            ordered[0] = (unsigned char)len_prefix;
+            auto cmp = [&](int i, int j) { return parent_prefix[i] < parent_prefix[j]; };
+            std::sort(&ordered[1], &ordered[len_prefix], cmp);
+
+            std::sort(parent_prefix.begin(), parent_prefix.end());
+            
+            unsigned short *pre_key = (unsigned short*) malloc(sizeof(unsigned short) * (len_prefix + 1));
+            pre_key[0] = (unsigned short)len_prefix;
+            memcpy(&pre_key[1], &parent_prefix[0], len_prefix * sizeof(unsigned short));
+            
+            prefix_key key = { pre_key };
+            N* child = NULL;
+            auto iter = pmap->find(key);
+            if (iter != pmap->end()) {
+                double permuted_lower_bound = iter->second.first;
+                if (lower_bound < permuted_lower_bound) {
+                    N* permuted_node;
+                    unsigned char* indices = iter->second.second;
+                    std::vector<unsigned short> permuted_prefix(parent_prefix.size());
+                    for (unsigned char i = 0; i < indices[0]; ++i)
+                        permuted_prefix[i] = parent_prefix[indices[i + 1]];
+
+                    if ((permuted_node = tree->check_prefix(permuted_prefix)) != NULL) {
+                        N* permuted_parent = permuted_node->parent();
+                        permuted_parent->delete_child(permuted_node->id());
+                        delete_subtree<N>(tree, permuted_node, false, true);
+                        logger.incPmapDiscardNum();
+                    } else {
+                        logger.incPmapNullNum();
+                    }
+                    child = construct_policy(new_rule, nrules, prediction,
+                                             default_prediction, lower_bound, objective,
+                                             parent, num_not_captured, nsamples,
+                                             len_prefix, c, minority);
+                    iter->second = std::make_pair(lower_bound, ordered);
+                }
+            } else {
+                child = construct_policy(new_rule, nrules, prediction,
+                                         default_prediction, lower_bound, objective,
+                                         parent, num_not_captured, nsamples, len_prefix,
+                                         c, minority);
+                unsigned char* ordered_prefix = &ordered[0];
+                pmap->insert(std::make_pair(key, std::make_pair(lower_bound, ordered_prefix)));
+                logger.incPmapSize();
+            }
+            return child;
+        }
+	private:
+		std::unordered_map<prefix_key, std::pair<double, unsigned char*>, prefix_hash>* pmap;
+};
+template<class N>
+PrefixPermutationMap<N>::PrefixPermutationMap()
+    : pmap(new std::unordered_map<prefix_key, std::pair<double, unsigned char*>, prefix_hash>) {}
+
+template<class N>
+class CapturedPermutationMap : public PermutationMap<N> {
+	public:
+        CapturedPermutationMap();
+        size_t size() override {
+            return cmap->size();
+        }
+        N* insert(construct_signature<N> construct_policy, unsigned short new_rule,
+					 size_t nrules, bool prediction, bool default_prediction, double lower_bound,
+					 double objective, N* parent, int num_not_captured, int nsamples, int len_prefix,
+					 double c, double minority, CacheTree<N>* tree, VECTOR not_captured,
+					 std::vector<unsigned short> parent_prefix)
+		{
+            parent_prefix.push_back(new_rule);
+            N* child = NULL;
+            captured_key key;
+            rule_vinit(nsamples, &key.key);
+            rule_copy(key.key, not_captured, nsamples);
+#ifndef GMP
+            key.len = (short) nsamples;
+#endif
+            auto iter = cmap->find(key);
+            if (iter != cmap->end()) {
+                std::vector<unsigned short> permuted_prefix = iter->second.first;
+                double permuted_lower_bound = iter->second.second;
+                if (lower_bound < permuted_lower_bound) {
+                    N* permuted_node;
+                    if ((permuted_node = tree->check_prefix(permuted_prefix)) != NULL) {
+                        N* permuted_parent = permuted_node->parent();
+                        permuted_parent->delete_child(permuted_node->id());
+                        delete_subtree<N>(tree, permuted_node, false, true);
+                        logger.incPmapDiscardNum();
+                    } else {
+                        logger.incPmapNullNum();
+                    }
+                    child = construct_policy(new_rule, nrules, prediction, default_prediction,
+                                               lower_bound, objective, parent,
+                                                num_not_captured, nsamples, len_prefix, c, minority);
+                    iter->second = std::make_pair(parent_prefix, lower_bound);
+                }
+            } else {
+                child = construct_policy(new_rule, nrules, prediction, default_prediction,
+                                            lower_bound, objective, parent,
+                                            num_not_captured, nsamples, len_prefix, c, minority);
+                cmap->insert(std::make_pair(key, std::make_pair(parent_prefix, lower_bound)));
+                logger.incPmapSize();
+            }
+            return child;
+		}
+ 
+	private:
+		std::unordered_map<captured_key, std::pair<std::vector<unsigned short>, double>, captured_hash>* cmap;
+};
+template<class N>
+CapturedPermutationMap<N>::CapturedPermutationMap()
+    : cmap(new std::unordered_map<captured_key, std::pair<std::vector<unsigned short>, double>, captured_hash>) {}
+
+
+template<class N>
+class NullPermutationMap : public PermutationMap<N>  {
+    public:
+        size_t size() {return 0;}
+};
+
+//typedef std::unordered_map<struct prefix_key, std::pair<double, unsigned char*>, prefix_hash> PrefixPermutationMap;
+//typedef std::unordered_map<struct captured_key, std::pair<std::vector<unsigned short>, double>, captured_hash> CapturedPermutationMap;
+
+//using pmap_garbage_collect_signature = void (*)(PermutationMap*, size_t);
+
+//void bfs_prefix_map_garbage_collect(PermutationMap* p, size_t min_length);
+//void prefix_map_garbage_collect(PermutationMap* p, size_t min_length);
+//void captured_map_garbage_collect(PermutationMap* p, size_t min_length);
